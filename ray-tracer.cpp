4225640ab2bd198cb189e848783fa3f5fd61da00
FredericoBresani <frederico@upplify.com>
2022-08-24 08:41:28 -0300

    inicial data structures started

--- ray-tracer.cpp
+++ ray-tracer.cpp
@@ -1,5 +1,51 @@
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
+
+// lets use doubles for object-ray intersection and floats for shading calculations
+
+template <typename T> class Vec2
+{
+    public:
+        T x, y;
+        Vec2(); // build all the vec2 operations inside this public section
+};
+
+template <typename T> class Vec3
+{
+    public:
+        T x, y, z;
+        Vec3(); //build all the vec3 operation inside this public section
+};
+
+typedef Vec2<float> vec2f;
+typedef Vec3<float> vec3f;
+
+class Object 
+{
+    public:
+        Object();
+        virtual int rayObjectIntersect();
+};
+
+class Sphere: public Object 
+{
+
+};
+
+class Triangle: public Object 
+{
+
+};
+
+class Plane: public Object 
+{
+
+};
+
+
+
+
 
 int main()
 {
