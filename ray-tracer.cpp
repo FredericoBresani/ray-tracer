775e08b7acab3f04b1989851c269338e84a5dcf8
FredericoBresani <frederico@upplify.com>
2022-09-24 19:32:54 -0300

    lines and triangles 0 division errors solved

--- ray-tracer.cpp
+++ ray-tracer.cpp
@@ -251,10 +251,107 @@ class Triangle: public Object
             Point3D pHit;
             if (tPlane->rayObjectIntersect(ray, tmin, info)) 
             {   
-                pHit = info.hit_location;
-                double gama = (pHit.z - ((pHit.x*A.z)/A.x) - ((((pHit.y*A.x) - (A.y*pHit.x))/((B.y*A.x) - (A.y*B.x)))*(B.z - (B.x*A.z)/A.x)))/((((A.y*C.x) - (C.y*A.x))/((B.y*A.x) - (A.y*B.x)))*(B.z - (B.x*A.z)/A.x) + (C.z - (C.x*A.z)/A.x));
-                double beta = ((pHit.y*A.x) - (gama*C.y*A.x) + (A.y*gama*C.x) - (A.y*pHit.x))/((B.y*A.x) - (A.y*B.x));
-                double alpha = ((pHit.x) - (beta*B.x) - (gama*C.x))/A.x;
+                pHit = info.hit_location;       
+                Vec3D temp;                     // _      _  _     _
+                Vec3D v0 = Vec3D(A.x, B.x, C.x);//|a1 b1 c1||a|   |X|
+                Vec3D v1 = Vec3D(A.y, B.y, C.y);//|a2 b2 c2||b| = |Y|
+                Vec3D v2 = Vec3D(A.z, B.z, C.z);//|a3 b3 c3||g|   |Z|
+                double point[] = { pHit.x, pHit.y, pHit.z };
+                double tempP;
+                if (v1.x != 0.0 && v0.x == 0.0)
+                {
+                    temp = v0;
+                    v0 = v1;
+                    v1 = temp;
+                    tempP = point[0];
+                    point[0] = point[1];
+                    point[1] = tempP;
+                }
+                else if(v2.x != 0.0 && v0.x == 0.0) 
+                {
+                    temp = v0;
+                    v0 = v2;
+                    v2 = temp;
+                    tempP = point[0];
+                    point[0] = point[2];
+                    point[2] = tempP;
+                }
+                if (v0.x != 0.0)
+                {
+                    point[1] = point[1] + (point[0]*(-1*(v1.x/v0.x)));
+                    point[2] = point[2] + (point[0]*(-1*(v2.x/v0.x)));
+                    v1 = v1 + v0*(-1*(v1.x/v0.x));
+                    v2 = v2 + v0*(-1*(v2.x/v0.x));
+                }
+                if (v2.y != 0.0 && v1.y == 0.0)
+                {
+                    temp = v1;
+                    v1 = v2;
+                    v2 = temp;
+                    tempP = point[1];
+                    point[1] = point[2];
+                    point[2] = tempP;
+                }
+                else if (v0.y != 0.0 && v1.y == 0.0 && v0.x == 0.0)
+                {
+                    temp = v1;
+                    v1 = v0;
+                    v0 = temp;
+                    tempP = point[1];
+                    point[1] = point[0];
+                    point[0] = tempP;
+                }
+                if (v1.y != 0.0)
+                {
+                    point[0] = point[0] + (point[1]*(-1*(v0.y/v1.y)));
+                    point[2] = point[2] + (point[1]*(-1*(v2.y/v1.y)));
+                    v0 = v0 + v1*(-1*(v0.y/v1.y));
+                    v2 = v2 + v1*(-1*(v2.y/v1.y));
+                }
+                if (v0.x == 0.0 && v0.y == 0.0 && v0.z != 0.0)
+                {
+                    temp = v2;
+                    v2 = v0;
+                    v0 = temp;
+                    tempP = point[2];
+                    point[2] = point[0];
+                    point[0] = tempP;
+                }
+                else if (v1.x == 0.0 && v1.y == 0.0 && v1.z != 0.0)
+                {
+                    temp = v2;
+                    v2 = v1;
+                    v1 = temp;
+                    tempP = point[2];
+                    point[2] = point[1];
+                    point[1] = tempP;
+                }
+                if (v2.z != 0.0)
+                {
+                    point[0] = point[0] + (point[2]*(-1*(v0.z/v2.z)));
+                    point[1] = point[1] + (point[2]*(-1*(v1.z/v2.z)));
+                    v0 = v0 + v2*(-1*(v0.z/v2.z));
+                    v1 = v1 + v2*(-1*(v1.z/v2.z));
+                }
+                double alpha = 0.0, beta = 0.0, gama = 0.0;
+                if (v2.z == 0.0 && (point[2] < -kEpsilon || point[2] > kEpsilon)) return (false);
+                if (v2.z != 0.0)
+                {
+                    gama = point[2]/v2.z; 
+                }
+                if (v1.y == 0.0 && (point[1] < -kEpsilon || point[1] > kEpsilon)) return (false);
+                if (v1.y != 0.0)
+                {
+                    beta = point[1]/v1.y;
+                }
+                if (v0.x == 0.0 && (point[0] < -kEpsilon || point[0] > kEpsilon)) return (false);
+                if (v0.x != 0.0)
+                {
+                    alpha = point[0]/v0.x;
+                }
+                if (alpha == 0.0) alpha = 1.0 - (beta + gama);
+                if (beta == 0.0) beta = 1.0 - (alpha + gama);
+                if (gama == 0.0) gama = 1.0 - (alpha + beta);
                 double ABGsum = alpha + beta + gama;
                 if (ABGsum <= 1.0 + kEpsilon && ABGsum >= 1.0 - kEpsilon)
                 {   
@@ -335,6 +432,11 @@ class Camera
             pixelQtnV = (double)vr/pixelsize;
             toScreen = toScreen.normalize(toScreen);  
             w = (toScreen*(-1.0))/toScreen.norma(toScreen);
+            Vec3D WUP = w ^ up;
+            if (WUP.x == 0.0 && WUP.y == 0.0 && WUP.x == 0.0)
+            {
+                up = Vec3D(1.0, 0.0, 0.0);
+            }
             u = u.normalize((w ^ up));
             v = u ^ w;
             right =  u*(2.0/pixelQtnH);
@@ -376,7 +478,7 @@ void render(std::vector<Object*>& objetos, std::vector<Light*>& lights, Camera& 
     std::vector<Vec3D> pixels;
     for (int i = 0; i < camera.pixelQtnH*camera.pixelQtnV; i++)
     {
-        if (i == 600*300 - 300)
+        if (i == 400*195 - 180)
         {
             int fr = 10;
         }
